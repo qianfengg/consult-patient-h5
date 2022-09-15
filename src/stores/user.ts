0882d8abc093d384da80bc5f82da1dbcbdd7d37b
qianfengg <fred_gqf0221@163.com>
2022-09-15 16:30:53 +0800

    用户状态仓库

---
+++ src/stores/user.ts
@@ -0,0 +1,19 @@
+import type { User } from '@/types/user'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+const useUserStore = defineStore('cp-user', () => {
+  const user = ref<User>({} as User)
+  const setUser = (u: User) => {
+    user.value = u
+  }
+  const delUser = () => {
+    user.value = {} as User
+  }
+  return {
+    user,
+    setUser,
+    delUser
+  }
+})
+export default useUserStore
---
+++ src/types/user.d.ts
@@ -0,0 +1,1 @@
+export type User = Record<'token' | 'avatar' | 'mobile' | 'account' | 'id', string>
