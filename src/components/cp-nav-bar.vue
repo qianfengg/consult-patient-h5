f4be4b1e95232343d4a7c3578c5f6cd7e305e0c7
qianfengg <fred_gqf0221@163.com>
2022-09-19 11:24:49 +0800

    cp-nav-bar 组件结构

---
+++ src/components/cp-nav-bar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+// 一定有的功能：返回图标，返回效果，固定定位（组件内部实现）
+// 使用组件时候才能确定的功能：标题，右侧文字，点击右侧文字行为（props传入）
+const onClickLeft = () => {
+  //
+}
+const onClickRight = () => {
+  //
+}
+</script>
+
+<template>
+  <van-nav-bar
+    left-arrow
+    @click-left="onClickLeft"
+    fixed
+    title="注册"
+    right-text="注册"
+    @click-right="onClickRight"
+  ></van-nav-bar>
+</template>
+
+<style lang="scss" scoped>
+::v-deep() {
+  .van-nav-bar {
+    &__arrow {
+      font-size: 18px;
+      color: var(--cp-text1);
+    }
+    &__text {
+      font-size: 15px;
+    }
+  }
+}
+</style>
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+// import CpNavBar from '../../components/cp-nav-bar.vue'
 // import { Button as VanButton, Checkbox as VanCheckbox } from 'vant'
 </script>
 
 <template>
   <div class="login-page">
+    <cp-nav-bar></cp-nav-bar>>
     <van-button type="primary">按钮</van-button>
     <van-checkbox :checked="true">复选框</van-checkbox>
     <van-icon name="chat-o" />
