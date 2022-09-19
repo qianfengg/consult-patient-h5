543abc916b324894978b2bdb0c210e2dd7accab7
qianfengg <fred_gqf0221@163.com>
2022-09-19 12:13:10 +0800

    cp-nav-bar 组件功能

--- src/components/cp-nav-bar.vue
+++ src/components/cp-nav-bar.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+defineProps<{
+  title?: string
+  rightText?: string
+}>()
+const emit = defineEmits<{
+  // eslint-disable-next-line no-unused-vars
+  (e: 'rightClick'): void
+}>()
+const router = useRouter()
 // 一定有的功能：返回图标，返回效果，固定定位（组件内部实现）
 // 使用组件时候才能确定的功能：标题，右侧文字，点击右侧文字行为（props传入）
 const onClickLeft = () => {
   //
+  const back = history.state.back
+  if (back) {
+    router.back()
+  } else {
+    router.push('/')
+  }
 }
 const onClickRight = () => {
   //
+  emit('rightClick')
 }
 </script>
 
@@ -14,8 +32,8 @@ const onClickRight = () => {
     left-arrow
     @click-left="onClickLeft"
     fixed
-    title="注册"
-    right-text="注册"
+    :title="title"
+    :right-text="rightText"
     @click-right="onClickRight"
   ></van-nav-bar>
 </template>
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 // import CpNavBar from '../../components/cp-nav-bar.vue'
 // import { Button as VanButton, Checkbox as VanCheckbox } from 'vant'
+const clickFn = () => {
+  console.log('click fn')
+}
 </script>
 
 <template>
   <div class="login-page">
-    <cp-nav-bar></cp-nav-bar>>
+    <cp-nav-bar title="测试title" right-text="测试右侧文字" @right-click="clickFn"></cp-nav-bar>>
     <van-button type="primary">按钮</van-button>
     <van-checkbox :checked="true">复选框</van-checkbox>
     <van-icon name="chat-o" />
