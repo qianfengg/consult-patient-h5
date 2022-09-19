ac50c3a64babfddb59307ff91722a98c16b82ffb
qianfengg <fred_gqf0221@163.com>
2022-09-19 14:44:10 +0800

    页面布局-头底

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,18 +1,56 @@
-<script setup lang="ts">
-// import CpNavBar from '../../components/cp-nav-bar.vue'
-// import { Button as VanButton, Checkbox as VanCheckbox } from 'vant'
-const clickFn = () => {
-  console.log('click fn')
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="login-page">
-    <cp-nav-bar title="测试title" right-text="测试右侧文字" @right-click="clickFn"></cp-nav-bar>>
-    <van-button type="primary">按钮</van-button>
-    <van-checkbox :checked="true">复选框</van-checkbox>
-    <van-icon name="chat-o" />
+    <cp-nav-bar right-text="注册" @click-right="$router.push('/register')"></cp-nav-bar>
+    <div class="login-head">
+      <h3>密码登录</h3>
+      <a href="javascript:;">
+        <span>短信验证码登录</span>
+        <van-icon name="arrow"></van-icon>
+      </a>
+    </div>
+    <!-- form 表单 -->
+    <div class="login-other">
+      <van-divider>第三方登录</van-divider>
+      <div class="icon">
+        <img src="@/assets/qq.svg" alt="" />
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.login {
+  &-page {
+    padding-top: 46px;
+  }
+  &-head {
+    padding: 30px 30px 50px;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    line-height: 1;
+    h3 {
+      font-weight: normal;
+      font-size: 24px;
+    }
+    a {
+      font-size: 15px;
+    }
+  }
+  &-other {
+    margin-top: 60px;
+    padding: 0 30px;
+    .icon {
+      display: flex;
+      justify-content: center;
+      img {
+        width: 36px;
+        height: 36px;
+        padding: 4px;
+      }
+    }
+  }
+}
+</style>
