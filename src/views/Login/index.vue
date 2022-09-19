620980a308f3ed60e89e8d8b9a05642e2ef48b19
qianfengg <fred_gqf0221@163.com>
2022-09-19 11:05:56 +0800

    vant组件自动按需加载

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { Button as VanButton, Checkbox as VanCheckbox } from 'vant'
+// import { Button as VanButton, Checkbox as VanCheckbox } from 'vant'
 </script>
 
 <template>
   <div class="login-page">
     <van-button type="primary">按钮</van-button>
     <van-checkbox :checked="true">复选框</van-checkbox>
+    <van-icon name="chat-o" />
   </div>
 </template>
 
