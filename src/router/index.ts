9c2d0747b73b637b97ab97a68e543ef377112c01
qianfengg <fred_gqf0221@163.com>
2022-09-19 10:41:40 +0800

    css变量主题定制

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{ path: '/login', component: () => import('@/views/Login/index.vue') }]
+  routes: [
+    { path: '/login', component: () => import('@/views/Login/index.vue') },
+    {
+      path: '/playground',
+      component: () => import('@/views/Playground/index.vue')
+    }
+  ]
 })
 
 export default router
---
+++ src/views/Playground/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Button as VanButton } from 'vant'
+</script>
+
+<template>
+  <div>Playground</div>
+  <h1>test h1 color</h1>
+  <div class="test-div"></div>
+  <div class="test-div"></div>
+  <van-button type="primary">test</van-button>
+</template>
+
+<style scoped lang="scss">
+h1 {
+  background-color: var(--test-color);
+}
+.test-div {
+  width: 200px;
+  height: 200px;
+  margin: 5px;
+}
+div:nth-of-type(2) {
+  background: var(--test-color);
+}
+div:nth-of-type(3) {
+  border: 1px solid var(--test-color);
+}
+</style>
