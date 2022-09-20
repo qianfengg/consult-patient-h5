94a42fd59ea93956ec621e0ffdb40ae23ed5072b
qianfengg <fred_gqf0221@163.com>
2022-09-20 14:47:40 +0800

    个人中心-用户信息类型

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -8,3 +8,17 @@ export type User = Record<'token' | 'avatar' | 'mobile' | 'account' | 'id', stri
  * bindMobile绑定三方登录
  */
 export type CodeType = 'login' | 'register' | 'changeMobile' | 'forgetPassword' | 'bindMobile'
+
+type OmitUser = Omit<User, 'token'>
+export type UserInfo = OmitUser & {
+  likeNumber: number
+  collectionNumber: number
+  score: number
+  couponNumber: number
+  orderInfo: {
+    paidNumber: number
+    receivedNumber: number
+    shippedNumber: number
+    finishedNumber: number
+  }
+}
