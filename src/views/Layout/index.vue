2bd1399c6714e149da971fc58275c029d1c7273f
qianfengg <fred_gqf0221@163.com>
2022-09-20 11:06:50 +0800

    底部tabbar处理

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,7 +3,38 @@
 <template>
   <div class="layout-page">
     <router-view></router-view>
+    <van-tabbar route>
+      <van-tabbar-item to="/home"
+        >首页<template #icon="{ active }">
+          <cp-icon :name="`home-index-${active ? 'active' : 'default'}`"></cp-icon> </template
+      ></van-tabbar-item>
+      <van-tabbar-item to="/article"
+        >健康百科<template #icon="{ active }">
+          <cp-icon :name="`home-article-${active ? 'active' : 'default'}`"></cp-icon> </template
+      ></van-tabbar-item>
+      <van-tabbar-item to="/notify"
+        >消息中心<template #icon="{ active }">
+          <cp-icon :name="`home-notice-${active ? 'active' : 'default'}`"></cp-icon> </template
+      ></van-tabbar-item>
+      <van-tabbar-item to="/user"
+        >我的<template #icon="{ active }">
+          <cp-icon :name="`home-mine-${active ? 'active' : 'default'}`"></cp-icon> </template
+      ></van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.layout-page {
+  ::v-deep() {
+    .van-tabbar-item {
+      &__icon {
+        font-size: 21px;
+      }
+      &__text {
+        font-size: 11px;
+      }
+    }
+  }
+}
+</style>
