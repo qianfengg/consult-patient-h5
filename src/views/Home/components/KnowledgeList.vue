84f34a8a157db53d82998570786c7afcb24abd44
qianfengg <fred_gqf0221@163.com>
2022-09-21 15:33:47 +0800

    知识加载效果

--- src/views/Home/components/KnowledgeList.vue
+++ src/views/Home/components/KnowledgeList.vue
@@ -1,10 +1,32 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import KnowledgeCard from './KnowledgeCard.vue'
+const loading = ref(false)
+const finished = ref(false)
+const list = ref<number[]>([])
+const onLoad = () => {
+  console.log('loading')
+  setTimeout(() => {
+    const data = [1, 2, 3, 4, 5]
+    list.value.push(...data)
+    loading.value = false
+    if (list.value.length > 20) {
+      finished.value = true
+    }
+  }, 1000)
+}
 </script>
 
 <template>
   <div class="knowledge-list">
-    <knowledge-card v-for="i in 5" :key="i"></knowledge-card>
+    <van-list
+      v-model:loading="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <knowledge-card v-for="i in list" :key="i"></knowledge-card>
+    </van-list>
   </div>
 </template>
 
