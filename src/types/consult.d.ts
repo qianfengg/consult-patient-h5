44b6862045a5b8f17e41bb1b888a1ceb1cccfb86
qianfengg <fred_gqf0221@163.com>
2022-09-21 15:48:21 +0800

    知识数据类型

--- src/types/consult.d.ts
+++ src/types/consult.d.ts
@@ -1,3 +1,38 @@
 // recommend推荐，fatReduction减脂，food健康饮食like关注医生页面文章
 // 文章类型
 export type KnowledgeType = 'like' | 'recommend' | 'fatReduction' | 'food'
+
+// 文章信息类型
+export type Knowledge = {
+  id: string
+  title: string
+  coverUrl: string[]
+  topics: string[]
+  collectionNumber: number
+  commentNumber: number
+  creatorName: string
+  creatorAvatar: string
+  creatorHospatalName: string
+  likeFlag: 0 | 1
+  content: string
+  creatorDep: string
+  creatorTitles: string
+  creatorId: string
+}
+
+// 文章列表
+export type KnowledgeList = Knowledge[]
+
+// 文章列表带分页
+export type KnowledgePage = {
+  pageTotal: number
+  total: number
+  rows: KnowledgeList
+}
+
+// 文章列表查询参数
+export type KnowledgeParams = {
+  type: KnowledgeType
+  current: number
+  pageSize: number
+}
