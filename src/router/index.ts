8424dc263faae28c25b5498efa0e1b9170c8a8f7
qianfengg <fred_gqf0221@163.com>
2022-09-29 19:18:34 +0800

    14-问诊室-支付失败

--- src/router/index.ts
+++ src/router/index.ts
@@ -74,7 +74,10 @@ const router = createRouter({
     {
       path: '/room',
       component: () => import('@/views/Room/index.vue'),
-      meta: { title: '问诊室' }
+      meta: { title: '问诊室' },
+      beforeEnter(to) {
+        if (to.query.payResult === 'false') return '/user/consult'
+      }
     },
     {
       path: '/user/consult',
