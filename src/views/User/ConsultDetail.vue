e410d0a52a4d02912ff226dd7c2d380e7a86bcf1
qianfengg <fred_gqf0221@163.com>
2022-10-01 10:13:51 +0800

    05-问诊记录-详情按钮处理

--- src/views/User/ConsultDetail.vue
+++ src/views/User/ConsultDetail.vue
@@ -66,13 +66,34 @@ onMounted(async () => {
         <van-cell title="实付款" :value="`￥${item.actualPayment}`" class="price" />
       </van-cell-group>
     </div>
-    <div class="detail-action van-hairline--top">
+    <div class="detail-time" v-if="item.status === OrderType.ConsultPay">
+      请在 <van-count-down :time="item.countdown * 1000" /> 内完成支付，超时订单将取消
+    </div>
+    <div class="detail-action van-hairline--top" v-if="item.status === OrderType.ConsultPay">
       <div class="price">
         <span>需付款</span>
         <span>￥{{ item.actualPayment }}</span>
       </div>
       <van-button type="default" round>取消问诊</van-button>
       <van-button type="primary" round>继续支付</van-button>
+    </div>
+    <div class="detail-action van-hairline--top" v-if="item.status === OrderType.ConsultWait">
+      <van-button type="default" round>取消问诊</van-button>
+      <van-button type="primary" round :to="`/room?orderId=${item.id}`">继续沟通</van-button>
+    </div>
+    <div class="detail-action van-hairline--top" v-if="item.status === OrderType.ConsultChat">
+      <van-button type="default" round v-if="item.prescriptionId">查看处方</van-button>
+      <van-button type="primary" round :to="`/room?orderId=${item.id}`">继续沟通</van-button>
+    </div>
+    <div class="detail-action van-hairline--top" v-if="item.status === OrderType.ConsultComplete">
+      <cp-consult-more></cp-consult-more>
+      <van-button type="default" round :to="`/room?orderId=${item.id}`">问诊记录</van-button>
+      <van-button type="primary" round v-if="item.evaluateId">写评价</van-button>
+      <van-button type="default" round v-else>查看评价</van-button>
+    </div>
+    <div class="detail-action van-hairline--top" v-if="item.status === OrderType.ConsultCancel">
+      <van-button type="default" round>删除订单</van-button>
+      <van-button type="primary" round to="/">咨询其他医生</van-button>
     </div>
   </div>
   <div class="consult-detail-page" v-else>
@@ -83,6 +104,23 @@ onMounted(async () => {
 </template>
 
 <style lang="scss" scoped>
+.detail-time {
+  position: fixed;
+  left: 0;
+  bottom: 65px;
+  width: 100%;
+  height: 44px;
+  background-color: #fff7eb;
+  text-align: center;
+  line-height: 44px;
+  font-size: 13px;
+  color: #f2994a;
+  .van-count-down {
+    display: inline;
+    color: #f2994a;
+  }
+}
+
 .consult-detail-page {
   padding: 46px 0 110px 0;
 }
